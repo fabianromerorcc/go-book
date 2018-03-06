952fb8a5c4cab61625d7ee75d9ab1a96863bee4d
Fabian Romero <faromero@uchile.cl>
2018-03-06 18:37:14 -0300

    Improved version of Server2 with the book. It was necessary to add a lock/unlock because every request is handled in a different goroutine, so many goroutines could access to the variable at the same time. Also, I had a nonsense if inside handler function, asking if the path is '/count' and never a request with that path gets forwarded to that function.

--- chapter1/server2/main.go
+++ chapter1/server2/main.go
@@ -1,28 +1,33 @@
-// Server2
+// Server2 is a minimal "echo" and counter server
 package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
+var mu sync.Mutex
 var count int
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", countHandler)
+	http.HandleFunc("/count", counter)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
+// handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/count" {
-		count++
-	}
+	mu.Lock()
+	count++
+	mu.Unlock()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
-func countHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Request count: %d\n", count)
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count: %d\n", count)
+	mu.Unlock()
 }
